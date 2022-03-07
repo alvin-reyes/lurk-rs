6a66b2bddbc81c38e8cb26b96f9724f3e10ba7cb
Anders Christiansen Sørby <anders@sorby.xyz>
2022-03-07 19:57:23 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -1,19 +1,14 @@
 {
-  description = "Lurk (rust)";
+  description = "TODO Description";
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
+    nixpkgs.url = github:nixos/nixpkgs;
     flake-utils = {
       url = github:numtide/flake-utils;
       inputs.nixpkgs.follows = "nixpkgs";
     };
     naersk = {
-      url = github:yatima-inc/naersk;
+      url = github:nix-community/naersk;
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    utils = {
-      url = github:yatima-inc/nix-utils;
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.naersk.follows = "naersk";
     };
   };
 
@@ -21,68 +16,73 @@
     { self
     , nixpkgs
     , flake-utils
-    , utils
     , naersk
     }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      supportedSystems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "i686-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-    in
-    flake-utils.lib.eachSystem supportedSystems (system:
-    let
-      lib = utils.lib.${system};
+      lib = nixpkgs.lib.${system};
       pkgs = nixpkgs.legacyPackages.${system};
-      inherit (lib) buildRustProject testRustProject getRust filterRustProject;
+      rustTools = import ./nix/rust.nix {
+        nixpkgs = pkgs;
+      };
+      getRust =
+        { channel ? "nightly"
+        , date
+        , sha256
+        , targets ? [ "wasm32-unknown-unknown" "wasm32-wasi" "wasm32-unknown-emscripten" ]
+        }: (rustTools.rustChannelOf {
+          inherit channel date sha256;
+        }).rust.override {
+          inherit targets;
+          extensions = [ "rust-src" "rust-analysis" ];
+        };
+      # This is the version used across projects
       rust = getRust { date = "2022-02-20"; sha256 = "sha256-ZptNrC/0Eyr0c3IiXVWTJbuprFHq6E1KfBgqjGQBIRs="; };
-      crateName = "lurk";
-      src = ./.;
-      buildInputs = with pkgs;
-        if !stdenv.isDarwin
-        then [ ocl-icd ]
-        else [
-          darwin.apple_sdk.frameworks.OpenCL
-        ];
-      project = buildRustProject {
-        root = ./.;
-        inherit src buildInputs;
+      # Get a naersk with the input rust version
+      naerskWithRust = rust: naersk.lib."${system}".override {
+        rustc = rust;
+        cargo = rust;
+      };
+      # Naersk using the default rust version
+      naerskDefault = naerskWithRust rust;
+      buildRustProject = pkgs.makeOverridable ({ rust, naersk ? naerskWithRust rust, ... } @ args: naersk.buildPackage ({
+        buildInputs = with pkgs; [ ];
+        targets = [ ];
         copyLibs = true;
+        remapPathPrefix =
+          true; # remove nix store references for a smaller output package
+      } // args));
+
+      # Convenient for running tests
+      testProject = buildRustProject { doCheck = true; inherit rust root; };
+      # Load a nightly rust. The hash takes precedence over the date so remember to set it to
+      # something like `lib.fakeSha256` when changing the date.
+      crateName = "my-crate";
+      root = ./.;
+      # This is a wrapper around naersk build
+      # Remember to add Cargo.lock to git for naersk to work
+      project = buildRustProject {
+        inherit root rust;
       };
       lurk-example = project.override {
         cargoBuildOptions = d: d ++ [ "--example lurk" ];
         copySources = [ "examples" "src" ];
         copyBins = true;
       };
-      packages = {
-        ${crateName} = project;
-        inherit lurk-example;
-      };
     in
     {
-      inherit packages;
-      checks = {
-        ${crateName} = project.override { doCheck = true; };
+      packages = {
+        ${crateName} = project;
+        "${crateName}-test" = testProject;
       };
 
       defaultPackage = self.packages.${system}.${crateName};
-
-      # To run with `nix run`
-      apps.lurk-example = flake-utils.lib.mkApp {
-        drv = lurk-example;
-        name = "lurk";
-      };
-
-      defaultApp = self.apps.${system}.lurk-example;
 
       # `nix develop`
       devShell = pkgs.mkShell {
         inputsFrom = builtins.attrValues self.packages.${system};
         nativeBuildInputs = [ rust ];
-        buildInputs = with pkgs; buildInputs ++ [
+        buildInputs = with pkgs; [
           rust-analyzer
           clippy
           rustfmt
